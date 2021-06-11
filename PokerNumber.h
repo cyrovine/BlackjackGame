39f2b42e824dc6e8b6b1bad285bc1a234b8d41d3
cyrovine <79899783+cyrovine@users.noreply.github.com>
2021-06-11 13:48:28 +0800

    Create PokerNumber.h

---
+++ PokerNumber.h
@@ -0,0 +1,17 @@
+#ifndef pokerNumber_h
+#define pokerNumber_h
+#include <string>
+#include"PokerSuits.h"
+
+using namespace std;
+
+class PokerNumber :public PokerSuits
+{
+public:
+	PokerNumber();
+	string getPokerNumber(int var);
+	string tostring(int var);
+private:
+	string pokerNumber[52]; //撲克名
+};
+#endif
